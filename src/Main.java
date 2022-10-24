9d27e8d1fa3c2077e17d4503a78badb947403e28
Szkolenie <agnieszka.strozyk@gmail.com>
2022-10-24 20:34:25 +0200

    dodanie wieku

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         String name="Aga";
-        System.out.println("Hello " + name);
+        int age=46;
+        System.out.println("Hello " + name +" mam lat " +age);
+
 
     }
 }
