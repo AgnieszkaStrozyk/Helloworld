4300d0285ecc8fc48d0ef8e617e403d3e56a9c13
Szkolenie <agnieszka.strozyk@gmail.com>
2022-10-24 20:59:35 +0200

    dodanie dwóch liczb

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        String name="Aga";
-        int age=46;
-        System.out.println("Hello " + name +" mam lat " +age);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Podej pierwszą wartość");
+        int number1= sc.nextInt();
+        System.out.println("Podej drugą wartość");
+        int number2= sc.nextInt();
+        System.out.println(number1 + number2);
+
 
 
     }
